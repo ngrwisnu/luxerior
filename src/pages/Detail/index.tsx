a5604cde78a9c1d67178fbc576d94af37febad62
ngrwisnu <adiprayogongrwisnu@gmail.com>
2023-01-11 18:52:03 +0700

    create detail page

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -3,6 +3,8 @@ import Footer from "../../components/groups/Footer/Footer";
 import Header from "../../components/groups/Header";
 import AsideMenu from "../../components/groups/AsideMenu/AsideMenu";
 import ClientsBrand from "../../components/groups/ClientsBrand/ClientsBrand";
+import ProductPreview from "../../components/groups/Detail/ProductPreview";
+import ProductSuggestions from "../../components/groups/Detail/ProductSuggestions";
 
 const Detail = () => {
   return (
@@ -15,6 +17,8 @@ const Detail = () => {
           { url: "/categories/1212/product/1123", name: "Details" },
         ]}
       />
+      <ProductPreview />
+      <ProductSuggestions />
       <ClientsBrand />
       <AsideMenu />
       <Footer />
